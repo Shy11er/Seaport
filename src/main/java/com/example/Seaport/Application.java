7351d9f878c1f91e5a3e7d894731d5d4f46047c9
sri@vt2b.ru <sri@vt2b.ru>
2023-12-07 11:33:05 +0500

    cors enable

--- src/main/java/com/example/Seaport/Application.java
+++ src/main/java/com/example/Seaport/Application.java
@@ -11,19 +11,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:5173")
-						.allowedMethods("GET", "POST", "PUT", "DELETE")
-						.allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept")
-						.allowCredentials(true)
-						.maxAge(3600);
-			}
-		};
-	}
 }
